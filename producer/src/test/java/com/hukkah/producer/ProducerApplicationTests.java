f8a667e1e4b86a40ba88e390f3c78fff4d54185a
mansoor-sajjad <mansoor.sajjad@hotmail.com>
2025-08-26 13:56:18 +0200

    Initialized producer.

---
+++ producer/src/test/java/com/hukkah/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hukkah.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
